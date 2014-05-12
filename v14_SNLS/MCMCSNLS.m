5b49d5af864adb65087d838729d12f6cf5fc748d
samy <kandasamy@cmu.edu>
2014-05-12 17:01:24 -0400

    made changes to v14 and v15

--- v14_SNLS/MCMCSNLS.m
+++ v14_SNLS/MCMCSNLS.m
@@ -1,19 +1,19 @@
 % Implements MCMC & MCMC Regression for the SNLS Experiment
 
-mcmcProposalStd = 0.5;
+mcmcProposalStd = 5;
 mcmcInitPt = 0.5*ones(numDims, 1); % init at centre point
-mcmcInitPt = [0.45; 0.24; 0.68]; % init at centre point
+mcmcInitPt = [0.4; 0.24; 0.68]; % init at centre point
 
 % Don't do Logit
 %%%%%%%%%%%%%%%%
-% % modify the log likelihood oracle to take the logit transform into account
-% mcmcEvalLogJoint = @(t) evalLogJoint(logitinv(t));
-% [logitMcmcSamples, logitMcmcQueries, mcmcLogProbs] = ...
-%   CustomMCMC(NUM_MCMC_SAMPLES, mcmcProposalStd, mcmcInitPt, mcmcEvalLogJoint);
-% mcmcSamples = logitinv(logitMcmcSamples);
-% mcmcQueries = logitinv(logitMcmcQueries);
-[mcmcSamples, mcmcQueries, mcmcLogProbs] = CustomMCMC(NUM_MCMC_SAMPLES, ...
-  mcmcProposalStd, mcmcInitPt, evalLogJoint);
+% modify the log likelihood oracle to take the logit transform into account
+mcmcEvalLogJoint = @(t) evalLogJoint(logitinv(t));
+[logitMcmcSamples, logitMcmcQueries, mcmcLogProbs] = CustomMCMC(...
+  NUM_MCMC_SAMPLES, mcmcProposalStd, logit(mcmcInitPt), mcmcEvalLogJoint);
+mcmcSamples = logitinv(logitMcmcSamples);
+mcmcQueries = logitinv(logitMcmcQueries);
+% [mcmcSamples, mcmcQueries, mcmcLogProbs] = CustomMCMC(NUM_MCMC_SAMPLES, ...
+%   mcmcProposalStd, mcmcInitPt, evalLogJoint);
 
 % Now perform MCMC
 for mcmcResIter = 1:numMCMCResultsToBeStored
